6b1a672430af4473c6c3c8ba5f7ec8e00687979e
Franklin Ramos <franklinramos@outlook.com>
2022-02-09 19:41:26 -0300

    Implementa handleClick

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import Categories from '../components/Categories';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 import ButtonShoppingCart from '../components/ButtonShoppingCart';
+import Card from '../components/Card';
 
 class Home extends Component {
   state = {
     categories: [],
+    // result: [],
   }
 
   async componentDidMount() {
     const categories = await getCategories();
-
     const categoriesMenu = categories.map((categorie, index) => (
       <label htmlFor={ index } key={ categorie.name } data-testid="category">
         { categorie.name }
@@ -20,11 +21,20 @@ class Home extends Component {
     this.setState({ categories: categoriesMenu });
   }
 
+  handleClick = async ({ target: { value } }) => {
+    const product = await getProductsFromCategoryAndQuery(value);
+
+    console.log(product);
+  }
+
   render() {
     const { categories } = this.state;
     return (
       <div>
-        <input type="text" />
+        <input data-testid="query-input" type="text" />
+        <button onClick={ this.handleClick } data-testid="query-button" type="button">
+          Pesquisar
+        </button>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
