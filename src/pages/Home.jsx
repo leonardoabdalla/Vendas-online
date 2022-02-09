52b639854efeb2812dbde766ddc19250339ba06b
diogonasci <diogonasci@hotmail.com>
2022-02-09 17:18:00 -0300

    Requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import Categories from '../Components/Categories';
+import { getCategories } from '../services/api.js';
 
 class Home extends Component {
+
+  state = {
+    categories: [],
+  }
+
+  async componentDidMount() {
+
+    const categories = await getCategories()
+
+     const categoriesMenu = categories.map((categorie) => <label key={ categorie.name } data-testid='category'>
+     { categorie.name }
+        <input type='radio' /> 
+        </label>)
+
+        this.setState( { categories: categoriesMenu })
+
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <input type="text" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Categories categories={ categories }  />
       </div>
     );
   }
